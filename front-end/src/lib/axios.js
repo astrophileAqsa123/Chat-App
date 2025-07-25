0f69702f7e05fd7e50aa065cf26bd02edbf9ac21
astrophileAqsa123 <aqsaarif5678@gmail.com>
2025-07-25 12:15:45 +0500

    Backend

--- front-end/src/lib/axios.js
+++ front-end/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance=axios.create({
-    baseURL:"http://localhost:5001/api",
+    baseURL:"https://91e519ac-4526-419f-aa5f-4a851dcbc638-00-1g0mmns9x9f6x.sisko.replit.dev/api",
     withCredentials:true,
 })
