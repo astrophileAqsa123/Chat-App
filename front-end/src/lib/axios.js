5c934c1757a0d5a4ed6ac88c0d2a4c9d36d40e2b
a_Aqsa123 <aqsaarif5678@gmail.com>
2026-03-16 20:58:11 +0500

    Update axios base URL for production environment

--- front-end/src/lib/axios.js
+++ front-end/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance=axios.create({
-    baseURL:"https://91e519ac-4526-419f-aa5f-4a851dcbc638-00-1g0mmns9x9f6x.sisko.replit.dev/api",
+    baseURL:"https://chat-app-production-f833.up.railway.app/api",
     withCredentials:true,
 })
