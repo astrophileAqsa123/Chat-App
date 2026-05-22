80316a5c2d141556eec062813923d2157ae6b37e
a_Aqsa123 <aqsaarif5678@gmail.com>
2026-05-22 17:11:05 +0500

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin:https://chat-app-chi-lilac-42.vercel.app/,
+    origin:"https://chat-app-chi-lilac-42.vercel.app",
     credentials: true,
   })
 );
