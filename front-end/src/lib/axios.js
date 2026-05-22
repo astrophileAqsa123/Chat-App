08d2d5976841255c3203ebad32e29d9f075a4c1c
a_Aqsa123 <aqsaarif5678@gmail.com>
2026-05-22 17:14:49 +0500

    Update axios baseURL to include '/api'

--- front-end/src/lib/axios.js
+++ front-end/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance=axios.create({
-    baseURL:"https://chat-app-q7up.onrender.com",
+    baseURL:"https://chat-app-q7up.onrender.com/api",
     withCredentials:true,
 })
