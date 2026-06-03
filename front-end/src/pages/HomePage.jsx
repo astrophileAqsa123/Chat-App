d671455ff4df1bb7479866518c7359fa940c9e88
a_Aqsa123 <aqsaarif5678@gmail.com>
2026-06-03 14:08:12 +0500

    Update HomePage.jsx

--- front-end/src/pages/HomePage.jsx
+++ front-end/src/pages/HomePage.jsx
@@ -1,21 +1,41 @@
 import { useChatStore } from "../store/useChatStore";
-
+import { useEffect } from "react";
 import Sidebar from "../components/Sidebar";
 import NoChatSelected from "../components/NoChatSelected";
 import ChatContainer from "../components/ChatContainer";
 
-
 const HomePage = () => {
-  const { selectedUser } = useChatStore();
+  const { 
+    selectedUser, 
+    subscribeToMessages, 
+    unsubscribeFromMessages,
+    users,
+    getUsers,
+    setSelectedUser
+  } = useChatStore();
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  // Auto-select first user once users are loaded
+  useEffect(() => {
+    if (users.length > 0 && !selectedUser) {
+      setSelectedUser(users[0]);
+    }
+  }, [users]);
+
+  useEffect(() => {
+    subscribeToMessages();
+    return () => unsubscribeFromMessages();
+  }, [selectedUser]);
 
   return (
     <div className="h-screen bg-base-200">
       <div className="flex items-center justify-center pt-20 px-4">
         <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
           <div className="flex h-full rounded-lg overflow-hidden">
-            
-            <Sidebar/>
-
+            <Sidebar />
             {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
           </div>
         </div>
@@ -23,4 +43,5 @@ const HomePage = () => {
     </div>
   );
 };
+
 export default HomePage;
